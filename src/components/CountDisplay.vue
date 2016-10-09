7e3c085fc4f50f42714f534b7148bb3d1da5edc1
Farhad <farhad@sent.com>
2016-10-09 14:11:37 +0200

    migrate vue1 to vue2

---
+++ src/components/CountDisplay.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    Count is {{ count }}
+  </div>
+</template>
+
+<script>
+import eventHub from '../eventHub.js'
+
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+
+  created () {
+    eventHub.$on('incCount', this.incCount)
+  },
+
+  methods: {
+    incCount () {
+      this.count++
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/IncButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <button type="button" @click.prevent="activate">+1</button>
+</template>
+
+<script>
+import eventHub from '../eventHub.js'
+
+export default {
+  methods: {
+    activate () {
+      eventHub.$emit('incCount')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
